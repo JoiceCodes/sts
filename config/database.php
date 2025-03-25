9d1ce02a56d19b4ca22f23e8c5f1feca074d1def
JesterByte <ejjose94@gmail.com>
2025-03-25 14:33:09 +0800

    Update database credentials in config/database.php

--- config/database.php
+++ config/database.php
@@ -1,10 +1,15 @@
 <?php
 date_default_timezone_set("Asia/Manila");
 
+// $host = "localhost";
+// $serverUsername = "root";
+// $serverPassword = "";
+// $databaseName = "sts_db";
+
 $host = "localhost";
-$serverUsername = "root";
-$serverPassword = "";
-$databaseName = "sts_db";
+$serverUsername = "u714551035_sts";
+$serverPassword = "6&6MCdMdxW>";
+$databaseName = "u714551035_sts_db";
 
 $connection = mysqli_connect($host, $serverUsername, $serverPassword, $databaseName);
 
